0fac87b60e019753f7ea9c59d818e5d99b8e285e
Kara Lu <karalu2@illinois.edu>
2025-08-30 21:15:20 -0500

    removed hold item anims

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jump;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
@@ -18,6 +23,23 @@ public class PlayerMovement : MonoBehaviour
             sr.flipX = false;
         else if (horizontalInput < -0.01f)
             sr.flipX = true;
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) && grounded)
+            Jump();
+
+        anim.SetBool("walk", horizontalInput != 0);
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, jump);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 
 }
